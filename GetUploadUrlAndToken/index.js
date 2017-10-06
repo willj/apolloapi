fb81059aa528d3d1cc493764debf4069bea79bde
Will <willj@users.noreply.github.com>
2017-10-06 14:17:25 +0100

    parse app settings to ints

--- GetUploadUrlAndToken/index.js
+++ GetUploadUrlAndToken/index.js
@@ -20,7 +20,7 @@ module.exports = function (context, req) {
 
         let startDate = new Date();
         let expiryDate = new Date(startDate);
-        expiryDate.setMinutes(startDate.getMinutes() + process.env.ApolloTokenDurationMinutes);
+        expiryDate.setMinutes(startDate.getMinutes() + parseInt(process.env.ApolloTokenDurationMinutes));
         
         let sharedAccessPolicy = {
             AccessPolicy: {
@@ -55,7 +55,7 @@ function isValidFileType(mimeType){
 }
 
 function isValidSize(fileSize){
-    return (fileSize <= process.env.ApolloMaxFileSizeBytes);
+    return (fileSize <= parseInt(process.env.ApolloMaxFileSizeBytes));
 }
 
 function generateBlobName(mimeType, fileName){
